3939413eb7aac97c8991ab6f15d0b70b34739f1e
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-07-01 17:14:33 +0500

    initialize Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The purpose of this project is to develop and optimize machine learning models for identifying semantically similar questions,utilizing MLflow for tracking and managing experiments. This involves preprocessing text data and evaluating the performance of different models',
+    author='Ubaid ur Rehman',
+    license='MIT',
+)
